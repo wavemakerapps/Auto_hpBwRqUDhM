10f12c633706e44b884639179d3876ef8f8a636e
root <root@d3a2e0704417>
2017-06-07 13:37:52 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_hpBwRqUDhM",
+  "homePage" : "Main",
+  "name" : "Auto_hpBwRqUDhM",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
